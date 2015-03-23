705704d73dcbf99711f7cad2a3ae9b3967bef60a
Damien Casoni <damiencasoni@gmail.com>
2015-03-23 22:20:08 +1100

    Tried a 'git add .' to get rid of a ~1 displayed by PoshGit

--- sqla.py
+++ sqla.py
@@ -13,7 +13,9 @@ cursor = conn.cursor()
 
 # create a table
 cursor.execute("""CREATE TABLE population
-                (city TEXT, state TEXT, population INT)
+                (city TEXT, 
+                state TEXT, 
+                population INT)
                 """)
 
 # close the database connection
