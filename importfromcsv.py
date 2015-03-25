eca6c64a7750837b20d02a6bafcfe887086eba16
Damien Casoni <damiencasoni@gmail.com>
2015-03-25 20:35:25 +1100

    Add importfromcsv.py & the CSV needed for this exercise

---
+++ importfromcsv.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# learn to import from CSV
+
+import csv
+import sqlite3
+
+with sqlite3.connect("new.db") as connection:
+    c = connection.cursor()
+    
+    # open the csv file and assign it to a variable
+    employees = csv.reader(open("employees.csv", "rU"))
+    
+    c.execute("DROP TABLE IF EXISTS employees")
+    # create a new table called employees
+    c.execute("CREATE TABLE employees(firstname TEXT, lastname TEXT)")
+    
+    # insert data into table
+    c.executemany('INSERT INTO employees(firstname, lastname) values (?, ?)', employees)
